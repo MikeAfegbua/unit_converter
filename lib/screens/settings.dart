c238379bdf5973a7c0ea0f87a818ea062fa5f47e
Micheal <aokhaimoh@yahoo.com>
2022-01-18 21:09:55 +0100

    finished settings page

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,33 +1,143 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
   static const id = 'settings.dart';
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool val = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: const Icon(Icons.arrow_back_ios),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+          ),
+        ),
         title: const Text('Settings'),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.more_vert,
+              size: 35,
+            ),
+          )
+        ],
       ),
       body: Column(
-        children: const [
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text(
+        children: [
+          ListTile(
+            leading: const Text(
               'Change Language',
               style: TextStyle(fontSize: 20),
             ),
+            trailing: Switch(
+                value: val,
+                onChanged: (var newVal) {
+                  setState(() {
+                    val = newVal;
+                  });
+                }),
           ),
-          Divider(),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text(
-              'Change Language',
+          const Divider(),
+          ListTile(
+            leading: const Text(
+              'Set auto night-mode',
               style: TextStyle(fontSize: 20),
             ),
+            trailing: Switch(
+                value: val,
+                onChanged: (var newVal) {
+                  setState(() {
+                    val = newVal;
+                  });
+                }),
           ),
+          const Divider(),
+          ListTile(
+            leading: const Text(
+              'Bip on key press',
+              style: TextStyle(fontSize: 20),
+            ),
+            trailing: Switch(
+                value: val,
+                onChanged: (var newVal) {
+                  setState(() {
+                    val = newVal;
+                  });
+                }),
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Text(
+              'Vibrate on key press',
+              style: TextStyle(fontSize: 20),
+            ),
+            trailing: Switch(
+                value: val,
+                onChanged: (var newVal) {
+                  setState(() {
+                    val = newVal;
+                  });
+                }),
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Text(
+              'Enable full screen',
+              style: TextStyle(fontSize: 20),
+            ),
+            trailing: Switch(
+                value: val,
+                onChanged: (var newVal) {
+                  setState(() {
+                    val = newVal;
+                  });
+                }),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          ListTile(
+            leading: const Text(
+              'Enable Double Zero',
+              style: TextStyle(fontSize: 20),
+            ),
+            trailing: Switch(
+                value: val,
+                onChanged: (var newVal) {
+                  setState(() {
+                    val = newVal;
+                  });
+                }),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          ListTile(
+            leading: const Text(
+              'Enable Triple Zero',
+              style: TextStyle(fontSize: 20),
+            ),
+            trailing: Switch(
+                value: val,
+                onChanged: (var newVal) {
+                  setState(() {
+                    val = newVal;
+                  });
+                }),
+          ),
+          const Divider(),
         ],
       ),
     );
